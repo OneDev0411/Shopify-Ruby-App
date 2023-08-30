405f87c39f0ab8ac9eb1426c9c76b0872017e37c
OneDev0411 <cryptoapple1109@gmail.com>
2023-08-30 17:53:21 -0300

    Fixing mobile styles

--- app/components/TitleBar.jsx
+++ app/components/TitleBar.jsx
@@ -5,8 +5,8 @@ export function TitleBar ({title, image, buttonText, handleButtonClick}) {
 		<>
       <div style={{borderBottom: '1px solid #E1E3E5'}}>
         <Grid>
-          <Grid.Cell columnSpan={{xs: 6, sm: 3, md: 6, lg: 10, xl: 6}}>
-            <div style={{display: 'flex', float: 'left', gap: '10px', alignItems: 'center', height: '36px'}}>
+          <Grid.Cell columnSpan={{xs: 6, sm: 4, md: 3, lg: 10, xl: 6}}>
+            <div className="title-bar-title">
               <img src={image} />
               <Text variant="headingLg" as="h1" fontWeight="medium">
                 {title}
@@ -14,8 +14,8 @@ export function TitleBar ({title, image, buttonText, handleButtonClick}) {
             </div>
           </Grid.Cell>
           {buttonText ? (
-            <Grid.Cell columnSpan={{xs: 6, sm: 3, md: 6, lg: 2, xl: 6}}>
-              <div style={{display: 'flex', justifyContent: 'end'}}>
+            <Grid.Cell columnSpan={{xs: 6, sm: 2, md: 3, lg: 2, xl: 6}}>
+              <div className="title-bar-button">
               <Button primary onClick={handleButtonClick}>{buttonText}</Button>
               </div>
           </Grid.Cell>
