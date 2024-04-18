72c366cd7dda87842d5000df0b104311c7a3aa1a
OneDev0411 <cryptoapple1109@gmail.com>
2024-04-18 15:41:52 +0000

    Delete unused pages

--- app/routes/NotFound.tsx
+++ app/routes/NotFound.tsx
@@ -1,21 +1,20 @@
 import { Card, EmptyState, Page } from "@shopify/polaris";
-import { notFoundImage } from "@assets";
 
 export default function NotFound() {
   return (
     <Page>
       <Card>
-        <Card.Section>
+        <div>
           <EmptyState
             heading="There is no page at this address"
-            image={notFoundImage}
+            image="https://assets.incartupsell.com/images/empty-state.svg"
           >
             <p>
               Check the URL and try again, or use the search bar to find what
               you need.
             </p>
           </EmptyState>
-        </Card.Section>
+        </div>
       </Card>
     </Page>
   );
